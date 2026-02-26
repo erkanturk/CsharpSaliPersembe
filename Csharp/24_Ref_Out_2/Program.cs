25f7609a3d8121c525d5f10b26c2d207c7ca999d
erkanturk <refres98@gmail.com>
2026-02-26 21:41:04 +0300

    out ref tuple

---
+++ Csharp/24_Ref_Out_2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _24_Ref_Out_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Out Parametresi 
+             * Ref de bir referans değeri gösterirken out parametresinde değer göstermemize gerek yoktur null bir yapıyı 
+             * Method içerisinde işleme tabi tutup null değerden çıkarırız ve bu yapıyı methodun dışına çıkarabiliriz.
+             
+             */
+            //int sayi;
+
+            //OutParametreli(out sayi, out int sayi2);
+            //Console.WriteLine(sayi);
+            //Console.WriteLine(sayi2);
+
+            Islem(out int x, 20);
+            Console.WriteLine(x);
+            long y = 1;
+            double z = 2;
+            Islem2(out y, out z);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(TupleMethod());
+            Islem3(out string deger, out char deger2);
+            Console.WriteLine(deger+" "+deger2);
+        }
+        static void OutParametreli(out int sayi, out int sayi2)
+        {
+            sayi = 20;
+            sayi2 = 30;
+        }
+        static void Islem(out int sayi, int sayi2)
+        {
+            sayi = sayi2;
+        }
+        static void Islem2(out long sayi, out double sayi2)
+        {
+            sayi = 123;
+            sayi2 = 123.456;
+        }
+        static void Islem3(out string metin, out char harf)
+        {
+            metin = "Out";
+            harf= 'A';
+        }
+        static (int, int,string) TupleMethod()
+        {
+            int x = 1;
+            int y = 2;
+            string metin = "Tuple";
+            return (x, y, metin);
+        }
+    }
+}
---
+++ Csharp/25_Recursive_Method/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _25_Recursive_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Recursive method
+             * Bir işin tamamını yapmak yerine bu işi parçalara ayırarak bir bütünü bozmadan çalışma yapısıdır.
+             * Bir methodun kendi içerisinde çağırılması sonucunda bu işlem gerçekleştirilir fakat 
+             * bu işlem için koşullu bir yapı olmalıdır.
+             
+             */
+            //Yaz();
+         
+            Console.WriteLine("Çoklu form için 1 e basın Tekli form için 2 ye basın");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi == 1)
+            {
+                Console.WriteLine("Kaç kişi kayıt edeceksin ?");
+                int kayit = Convert.ToInt32(Console.ReadLine());
+                CokluForm(kayit-1);
+            }
+            else if (sayi == 2)
+            {
+                Form();
+            }
+            else
+            {
+                Console.WriteLine("Hatalı tuşlama");
+            }
+
+        }
+        static void Yaz()//hatalı yazım system overflow hatası verir.
+        {
+            Console.WriteLine("Merhaba");
+            Yaz();
+        }
+        static void Form()
+        {
+            Console.WriteLine("Ad");
+            string ad = Console.ReadLine();
+            Console.WriteLine("Soyad");
+            string soyad = Console.ReadLine();
+            Console.WriteLine("Yaş");
+            int yas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Ad: {ad} Soyad: {soyad} Yaş: {yas}");
+        }
+        static void CokluForm(int sayi)//Recursive method yapı LİFO (Last in First out) son giren ilk çıkar mantığı ile çalışıyor.
+        {
+            Console.WriteLine("Ad");
+            string ad = Console.ReadLine();
+            Console.WriteLine("Soyad");
+            string soyad = Console.ReadLine();
+            Console.WriteLine("Yaş");
+            int yas = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi > 0)
+            {
+                CokluForm(sayi - 1);
+            }
+            Console.WriteLine($"Ad: {ad} Soyad: {soyad} Yaş: {yas}");
+        }
+    }
+}
